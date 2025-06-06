698cf746e37644cbda4b4327e8006ad6b0013c6d
Nikita Bystritsky <nbystritsky@gmail.com>
2025-06-06 21:13:47 +0500

    feat: module4-task1 done

---
+++ src/components/offer-card/offer-card.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Offer } from '../../types/offer';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../constants/routers';
+
+interface OfferCardProps {
+  offer: Offer;
+}
+
+const OfferCard: React.FC<OfferCardProps> = ({offer}) => (
+  <article className="cities__card place-card">
+    {offer.isPremium && (
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+    )}
+    <div className="cities__image-wrapper place-card__image-wrapper">
+      <Link to={`${AppRoutes.Offer}/:${offer.id}`}>
+        <img
+          className="place-card__image"
+          src={`img/${offer.img}`}
+          width="260"
+          height="200"
+          alt={offer.name}
+        />
+      </Link>
+    </div>
+    <div className="place-card__info">
+      <div className="place-card__price-wrapper">
+        <div className="place-card__price">
+          <b className="place-card__price-value">&euro;{offer.price}</b>
+          <span className="place-card__price-text"> &#47;&nbsp;night</span>
+        </div>
+        <button className="place-card__bookmark-button button" type="button">
+          <svg className="place-card__bookmark-icon" width="18" height="19">
+            <use xlinkHref="#icon-bookmark"></use>
+          </svg>
+          <span className="visually-hidden">To bookmarks</span>
+        </button>
+      </div>
+      <div className="place-card__rating rating">
+        <div className="place-card__stars rating__stars">
+          <span style={{ width: `${(offer.rating / 5) * 100}%`}}></span>
+          <span className="visually-hidden">Rating</span>
+        </div>
+      </div>
+      <h2 className="place-card__name">
+        <a href="#">{offer.name}</a>
+      </h2>
+      <p className="place-card__type">{offer.type}</p>
+    </div>
+  </article>
+);
+
+export default OfferCard;
---
+++ src/pages/favorites-page/favorites-page.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Offer } from '../../types/offer';
+import FavoritesOffersList from '../../components/favorites-offers-list/favorites-offers-list';
+import { Header } from '../../components/header/header';
+import { AppRoutes } from '../../constants/routers';
+
+interface FavoritesProps {
+  offers: Offer[];
+}
+
+const FavoritesPage: React.FC<FavoritesProps> = ({ offers }) => {
+  const cities = [...new Set(offers.map((offer) => offer.city))];
+
+  return (
+    <div className="page">
+      { Header }
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {
+                cities.map((city) =>(
+                  <li className="favorites__locations-items" key={`${city}-items`}>
+                    <div className="favorites__locations locations locations--current">
+                      <div className="locations__item">
+                        <a className="locations__item-link" href="#">
+                          <span>{city}</span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className="favorites__places">
+                      {
+                        <FavoritesOffersList offers={offers.filter((offer) => offer.city === city)} />
+                      }
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+        </div>
+      </main>
+      <footer className="footer container">
+        <Link className="header__logo-link" to={AppRoutes.Default}>
+          <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </Link>
+      </footer>
+    </div>
+  );
+};
+
+export default FavoritesPage;
